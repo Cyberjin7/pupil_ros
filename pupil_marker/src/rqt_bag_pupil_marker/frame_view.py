2744b32a51bb2b419f4a58f79ced78d3b9d6a7eb
Jin <jinholee727@gmail.com>
2022-05-20 02:13:11 +0200

    Can scroll through multiple topics in one view

--- pupil_marker/src/rqt_bag_pupil_marker/frame_view.py
+++ pupil_marker/src/rqt_bag_pupil_marker/frame_view.py
@@ -8,12 +8,15 @@ from PIL.ImageQt import ImageQt
 import sys
 
 from python_qt_binding import QT_BINDING_MODULES
-from python_qt_binding.QtGui import QPixmap
+from python_qt_binding.QtGui import QPen, QBrush
 from python_qt_binding.QtWidgets import QGraphicsScene, QGraphicsView, QPushButton, QGroupBox, QVBoxLayout, QHBoxLayout
+from python_qt_binding.QtCore import QRectF
 
 # for code completion. Comment out when running code
-from PyQt5.QtGui import QPixmap
+# from PyQt5.QtGui import QPixmap
 # from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QPushButton, QGroupBox
+from PyQt5.QtCore import QRectF
+from PyQt5.QtGui import QPen, QBrush
 
 
 class FrameView(ImageView):
@@ -21,7 +24,7 @@ class FrameView(ImageView):
 
     def __init__(self, timeline, parent, topic):
         # super(FrameView, self).__init__(timeline, parent, topic)
-        TopicMessageView.__init__(self, timeline, parent, topic)
+        # TopicMessageView.__init__(self, timeline, parent, topic)
         self._image = None
         self._image_topic = None
         self._image_stamp = None
@@ -35,7 +38,7 @@ class FrameView(ImageView):
         self._overlay_indent = (4, 4)
         self._overlay_color = (0.2, 0.2, 1.0)
 
-        self.make_box('1')
+        # self.make_box('1')
 
         self._image_view = QGraphicsView(parent)
         self._image_view.resizeEvent = self._resizeEvent
@@ -43,14 +46,16 @@ class FrameView(ImageView):
         self._image_view.setScene(self._scene)
         parent.layout().addWidget(self._image_view)
         parent.layout().addWidget(self.message_tree)
+        TopicMessageView.__init__(self, timeline, parent, topic)
         parent.layout().addWidget(QPushButton('Export Markers'))
-        parent.layout().addWidget(self.box1)
-        print(type(parent))
+        # parent.layout().addWidget(self.box1)
+        # print(type(parent))
+        self.topics = timeline._get_topics()  # once I know which topics are needed, will filter list accordingly
 
     def make_box(self, name):
         self.box1 = QGroupBox(name)
 
-        layout = QVBoxLayout()
+        layout = QHBoxLayout()
         layout.addWidget(QPushButton("1"))
         layout.addWidget(QPushButton("2"))
         self.box1.setLayout(layout)
@@ -65,7 +70,11 @@ class FrameView(ImageView):
         if not msg:
             self.set_image(None, topic, 'no message')
         else:
-            self.set_image(msg.image, topic, t)  # temporary time until I implement synced time
+            if topic == 'pupil_world':
+                self.set_image(msg.image, topic, t)  # temporary time until I implement synced time
+            elif topic == 'pupil_gaze':
+                self._scene.addEllipse(QRectF(50, 50, 10, 10), QPen(), QBrush())
+
         if t is None:
             self.message_cleared()
         else:
@@ -74,6 +83,39 @@ class FrameView(ImageView):
     def message_cleared(self):
         super(ImageView, self).message_cleared()
 
+    def navigate_next(self):
+        for bag, entry in self.timeline.get_entries_with_bags(self.topics, self.timeline._timeline_frame.playhead, self.timeline._timeline_frame.end_stamp):
+            if entry.time > self.timeline._timeline_frame.playhead:
+                self.message_viewed(bag, self.timeline.read_message(bag, entry.position))
+                self.timeline._timeline_frame.playhead = entry.time
+                break
 
+    def navigate_previous(self):
+        last_entry = None
+        last_bag = None
+        for bag, entry in self.timeline.get_entries_with_bags(self.topics, self.timeline._timeline_frame.start_stamp,
+                                               self.timeline._timeline_frame.playhead):
+            if entry.time < self.timeline._timeline_frame.playhead:
+                last_entry = entry
+                last_bag = bag
 
+        if last_entry:
+            self.message_viewed(last_bag, self.timeline.read_message(last_bag, last_entry.position))
+            self.timeline._timeline_frame.playhead = last_entry.time
 
+    def navigate_first(self):
+        for bag, entry in self.timeline.get_entries_with_bags(self.topics, *self.timeline._timeline_frame.play_region):
+            self.message_viewed(bag, self.timeline.read_message(bag, entry.position))
+            self.timeline._timeline_frame.playhead = entry.time
+            break
+
+    def navigate_last(self):
+        last_entry = None
+        last_bag = None
+        for bag, entry in self.timeline.get_entries_with_bags(self.topics, *self.timeline._timeline_frame.play_region):
+            last_entry = entry
+            last_bag = bag
+
+        if last_entry:
+            self.message_viewed(last_bag, self.timeline.read_message(last_bag, last_entry.position))
+            self.timeline._timeline_frame.playhead = last_entry.time
