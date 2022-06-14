0c82a13bf21998c37acde53cf6c30c8eeeb339dd
Jin <jinholee727@gmail.com>
2022-06-15 01:15:49 +0200

    Add GUI for markers

--- pupil_marker/src/rqt_bag_pupil_marker/gaze_view.py
+++ pupil_marker/src/rqt_bag_pupil_marker/gaze_view.py
@@ -1,11 +1,11 @@
 from rqt_bag_plugins.image_view import ImageView
 from rqt_bag import TopicMessageView
 
-from PIL import Image
-from PIL.ImageQt import ImageQt
+# from PIL import Image
+# from PIL.ImageQt import ImageQt
 
 from python_qt_binding.QtGui import QPen, QBrush, QPixmap
-from python_qt_binding.QtWidgets import QGraphicsScene, QGraphicsView, QPushButton, QGroupBox, QVBoxLayout, QHBoxLayout, QApplication
+from python_qt_binding.QtWidgets import QGraphicsScene, QGraphicsView, QPushButton, QGroupBox, QVBoxLayout, QHBoxLayout, QApplication, QGraphicsPixmapItem, QComboBox, QLabel
 from python_qt_binding.QtCore import QRectF, Qt
 
 # for code completion. Comment out when running code
@@ -13,7 +13,7 @@ from python_qt_binding.QtCore import QRectF, Qt
 # from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QPushButton, QGroupBox
 from PyQt5.QtCore import QRectF, Qt
 from PyQt5.QtGui import QPen, QBrush, QPixmap
-from PyQt5.QtWidgets import QApplication, QGraphicsPixmapItem
+from PyQt5.QtWidgets import QApplication, QGraphicsPixmapItem, QComboBox, QLabel
 
 
 
@@ -23,6 +23,37 @@ class GazeView(ImageView):
     def __init__(self, timeline, parent, topic):
         # super(GazeView, self).__init__(timeline, parent, topic)
         TopicMessageView.__init__(self, timeline, parent, topic)
+
+        self.label_Layout = QHBoxLayout()
+        self.label_Layout.addWidget(QLabel("Marker:"))
+        self.labels_box = QComboBox()
+        self.labels_box.addItems(['Target Start',
+                                  'Target End',
+                                  'Goal Start',
+                                  'Goal End',
+                                  'Experiment Start',
+                                  'Experiment End'])
+        #self.labels_box.setPlaceholderText('None')
+        # self.labels_box.currentTextChanged.connect(self.test2)
+        # self.labels_box.activated.connect(self.test1)
+
+        self.label_Layout.addWidget(self.labels_box)
+        parent.layout().addLayout(self.label_Layout)
+
+        self.remove_button = QPushButton('Remove')
+        self.remove_button.setCheckable(True)
+        self.remove_button.setChecked(True)
+        self.remove_button.setEnabled(False)
+        self.add_button = QPushButton('Add')
+        self.add_button.clicked.connect(self.add_marker)
+
+        self.button_Layout = QHBoxLayout()
+        self.button_Layout.addWidget(self.remove_button)
+        self.button_Layout.addWidget(self.add_button)
+        parent.layout().addLayout(self.button_Layout)
+
+        self.labels_box.setCurrentText('Experiment Start')
+        self.labels_box.setCurrentIndex(-1)
 
         self.pen = QPen()
         self.brush = QBrush(Qt.green, Qt.SolidPattern)
@@ -35,13 +66,24 @@ class GazeView(ImageView):
 
         self.gaze_size = 30
 
+        self.markers = []  # TODO: Use numpy array instead. Or maybe stick to list for markers since string?
+        self.timestamps = []  # TODO: Use numpy array instead
+        self.msg_timestamp = 0.0
+
         # print(self.popup_keys)
         # print(self._image_view)
         # print(self._scene)
 
+    def add_marker(self):
+        marker = self.labels_box.currentText()
+        # print(marker)
+        if self.msg_timestamp not in self.timestamps and marker != "":
+            self.timestamps.append(self.msg_timestamp)  # placeholder code
+
     def message_viewed(self, bag, msg_details):
         TopicMessageView.message_viewed(self, bag, msg_details)
         topic, msg, t = msg_details[:3]
+        self.msg_timestamp = msg.timestamp  # alternatively: self.msg_timestamp = t.to_sec()
 
         # Currently assumes only one pixmap for image drawn
         scene_items = self._scene.items(Qt.DescendingOrder)
